f44336573d7c593cc61851e9d9c8f86fa2075685
Adam Yusron <88300589+Adamyr159@users.noreply.github.com>
2022-02-25 19:30:10 +0700

    Semester 2 (PHP

---
+++ Praktikum 1/variable_user.php
@@ -0,0 +1,11 @@
+<?php 
+$nama = "Adam";
+$umur = 17;
+$berat = 55;
+
+echo "halo nama saya $nama";
+echo "<br>Umur : $umur th";
+echo "<br>Berat : $berat kg";
+
+echo "<br> Hello $nama Apa Kabar?";
+?>
